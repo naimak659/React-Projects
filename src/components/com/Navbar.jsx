6de8839b8167a1f09414785692b252b53f148247
Naim AK <naimak659@gmail.com>
2024-04-14 20:40:12 +0600

    added shadCn and also create Navbar

---
+++ src/components/com/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="flex">
+        <h1>Navbar</h1>
+        <Button>Hello</Button>
+      </div>
+    </>
+  );
+}
